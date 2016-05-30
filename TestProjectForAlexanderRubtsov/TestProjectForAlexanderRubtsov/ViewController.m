056694d478cd0aa3f5de0a0ce3a9279869ce609c
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-05-30 12:38:06 +0300

    Реализовал вывод списка студентов

--- TestProjectForAlexanderRubtsov/TestProjectForAlexanderRubtsov/ViewController.m
+++ TestProjectForAlexanderRubtsov/TestProjectForAlexanderRubtsov/ViewController.m
@@ -7,21 +7,98 @@
 //
 
 #import "ViewController.h"
+#import "SiSStudent.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) NSMutableArray* students;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (instancetype)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+    }
+    return self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"Студенты";
+    
+    UIBarButtonItem* addingStudent = [[UIBarButtonItem alloc]
+                                      initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                      target:self
+                                      action:@selector(actionAdd:)];
+    
+    
+    self.navigationItem.rightBarButtonItems = @[addingStudent];
+    
+    self.students = [NSMutableArray array];
+    
+    for (NSUInteger i = 0; i < 5; i++) {
+        
+        SiSStudent* student = [[SiSStudent alloc] init];
+        [self.students addObject:student];
+    }
+    
+}
+
+#pragma mark - === ACTIONS ===
+
+- (void) actionAdd:(UIBarButtonItem*) sender {
+    
+    SiSStudent* student = [[SiSStudent alloc] init];
+    
+    [self.students addObject:student];
+    
+    [self.tableView reloadData];
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.students count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString* identifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                      reuseIdentifier:identifier];
+    }
+    SiSStudent* student = [self.students objectAtIndex:indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@", student.firstName, student.lastName];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%.2f", student.grade];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+}
+
+#pragma mark - UITableView Delegate
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    SiSStudent* student = [self.students objectAtIndex:indexPath.row];
+    SiSAddStudentViewController* vc = [[SiSAddStudentViewController alloc] init];
+    vc.student = student;
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 @end
